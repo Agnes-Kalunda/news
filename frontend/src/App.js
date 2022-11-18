1eb61831d91ef8435e6c503e573c293cd4cba278
Agnes-Kalunda <agypeter97@gmail.com>
2022-11-19 01:50:42 +0300

    testing frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar />
+     <Router>
+      
+       <Routes>
+       
+         <Route exact path="/">
+           <News key={"general"} pageSize={9} country="in" category="general" />
+         </Route>
+         <Route exact path="/Business">
+           <News key={"business"} pageSize={9} country="in" category="business" />
+         </Route>
+         <Route exact path="/Science">
+           <News key={"science"} pageSize={9} country="in" category="science" />
+         </Route>
+         <Route exact path="/Entertainment">
+           <News key={"entertainment"} pageSize={9} country="in" category="entertainment" />
+         </Route>
+         <Route exact path="/Sports">
+           <News key={"sports"} pageSize={9} country="in" category="sports" />
+         </Route>
+         <Route exact path="/Tech">
+           <News key={"tech"} pageSize={9} country="in" category="technology" />
+         </Route>
+         <Route exact path="/Health">
+           <News  key={"health"} pageSize={9} country="in" category="health" />
+         </Route>
+       </Routes>
+     </Router>
+   </>
   );
 }
 
---
+++ frontend/src/components/Loading.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+export class Loading extends Component {
+  render() {
+    return (
+      <div className="text-center">
+        <h2>Please hold on a sec....</h2>
+      </div>
+    );
+  }
+}
+
+export default Loading;
